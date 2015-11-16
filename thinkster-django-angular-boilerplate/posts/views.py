93d1797e5387603d0946af6dab94a909716b3fc6
jeffthemaximum <frey.maxim@gmail.com>
2015-11-16 12:40:24 -0500

    Make a PostViewSet viewset, Make an AccountPostsViewSet viewset

--- thinkster-django-angular-boilerplate/posts/views.py
+++ thinkster-django-angular-boilerplate/posts/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from rest_framework.response import Response
+from post.models import Post
+from posts.permissions import IsAuthorOfPost
+from post.serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.order_by('-created_at')
+    serializer_class = PostSerializer
+
+    def get_permissions(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return (permissions.AllowAny(),)
+        return(permissions.IsAuthenticated(), IsAuthorOfPost(),)
+
+
+def perform_create(self, serializer):
+    instance = serializer.save(author=self.request.user)
+
+    return super(PostViewSet, self).perform_create(serializer)
+
+
+class AccountPostsViewSet(viewsets.viewset):
+    queryset = Post.objects.select_related('author').all()
+    serializer_class = PostSerializer
+
+    def list(self, request, account_username=None):
+        queryset = self.queryset.filter(author_username=account_username)
+        serializer = self.serializer_class(queryset, many=True)
+
+        return Response(serializer.data)
