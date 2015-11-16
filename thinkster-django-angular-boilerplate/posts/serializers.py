7964f046e401b708cf2341ce614be3de3805c250
jeffthemaximum <frey.maxim@gmail.com>
2015-11-16 13:00:44 -0500

    Define the thinkster.posts module

--- thinkster-django-angular-boilerplate/posts/serializers.py
+++ thinkster-django-angular-boilerplate/posts/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 
 from authentication.serializers import AccountSerializer
-from post.models import Post
+from posts.models import Post
 
 
-class PostSerializer(serializers.ModelSerialzer):
+class PostSerializer(serializers.ModelSerializer):
     author = AccountSerializer(read_only=True, required=False)
 
     class Meta:
