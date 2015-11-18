9d5971bcd888923e1cfefd81d438dc29e26289bb
jeffthemaximum <frey.maxim@gmail.com>
2015-11-17 19:01:25 -0500

    Make a route for viewing user profiles

--- thinkster-django-angular-boilerplate/static/javascripts/thinkster.routes.js
+++ thinkster-django-angular-boilerplate/static/javascripts/thinkster.routes.js
@@ -27,6 +27,11 @@
       controllerAs: 'vm',
       templateUrl: '/static/templates/layout/index.html'
     })
+    .when('/+:username', {
+      controller: 'ProfileController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/profiles/profile.html'
+    })
     .otherwise('/');
   }
 })();
